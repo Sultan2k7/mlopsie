e1e3e5c659a485b65a839eb3b83a91f212ed3c4a
Sultan2k7 <sarysultanbek2007@gmail.com>
2025-07-09 17:59:50 +0500

    Add CI/CD

--- ml_service/extractor.py
+++ ml_service/extractor.py
@@ -1,6 +1,6 @@
 # extractor.py
 # Placeholder for hashtag and mention extraction logic 
-from transformers import pipeline
+from transformers import pipeline # type: ignore
 
 # Load the sentiment analysis pipeline once at startup
 sentiment_pipeline = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
